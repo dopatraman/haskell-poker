a3d0c5d795763150820072d8661ff820b59b8cf1
Prakash Venkat <venkatraman.prakash@gmail.com>
2018-07-28 12:58:29 -0700

    poker stubs

--- src/Data/Hand/Hand.hs
+++ src/Data/Hand/Hand.hs
@@ -1,6 +1,7 @@
 module Data.Hand.Hand  where
 
 import Data.Card.Card
+import Data.Hand.Unigraph
 
 type Unsorted = (Card, Card, Card, Card, Card)
 data Hand = HighCard CardName
@@ -12,12 +13,20 @@ data Hand = HighCard CardName
             | Flush
             | StraightFlush Card deriving (Eq, Ord, Show)
 
-
-highestHand :: (Card, Card, Card, Card, Card) -> (Card, Card) -> Hand
-highestHand community player = undefined
+playedHand :: (Card, Card, Card, Card, Card) -> (Card, Card) -> Hand
+playedHand community player = maximum $ map highestHand (unsortedHands community player)
 
 unsortedHands :: (Card, Card, Card, Card, Card) -> (Card, Card) -> [Unsorted]
 unsortedHands community player = undefined
 
+highestHand :: Unsorted -> Hand
+highestHand u = maximum $ possibleHands u
+
 possibleHands :: Unsorted -> [Hand]
-possibleHands u = undefined
+possibleHands u = getValues $ filterNothings $ pipeline $ toList u
+    where   cardFns = undefined
+            toList tup = undefined
+            pipeline unsorted = map ($ unsorted) cardFns
+            filterNothings maybeList = filter (\x -> x /= Nothing) maybeList
+            getValues maybeList = map just maybeList
+            just m = case m of Just a -> a
